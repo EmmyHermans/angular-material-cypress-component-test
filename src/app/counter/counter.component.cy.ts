5ce76c8c5ba5d2bc8d9e7b7715378967a048336c
Emmy Hermans <emmy.hermans@amis.nl>
2024-07-23 16:02:23 +0200

    Add custom cypress command for selecting elements with data-cy attribute

--- src/app/counter/counter.component.cy.ts
+++ src/app/counter/counter.component.cy.ts
@@ -3,6 +3,6 @@ import { CounterComponent } from './counter.component';
 describe(CounterComponent.name, () => {
   it('renders with the counter on 0', () => {
     cy.mount(CounterComponent);
-    cy.get('[data-cy=count]').should('have.text', '0');
+    cy.getBySelector('count').should('have.text', '0');
   });
 });
