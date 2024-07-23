2b32eb88567fa04acb6a6567778abc876dac1fda
Emmy Hermans <emmy.hermans@amis.nl>
2024-07-23 15:08:15 +0200

    added counter to app

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './counter.component.html',
-  styleUrl: './counter.component.css'
+  styleUrl: './counter.component.css',
 })
 export class CounterComponent {
+  count = 0;
 
+  public increment(): void {
+    this.count++;
+  }
+
+  public decrement(): void {
+    this.count--;
+  }
 }
