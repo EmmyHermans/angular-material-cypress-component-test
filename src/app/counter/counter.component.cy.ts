0c0dcbf384e7934198181545fd0fa7c69e6cc2ff
Emmy Hermans <emmy.hermans@amis.nl>
2024-07-23 16:06:03 +0200

    added additional tests for the counter component

--- src/app/counter/counter.component.cy.ts
+++ src/app/counter/counter.component.cy.ts
@@ -5,4 +5,20 @@ describe(CounterComponent.name, () => {
     cy.mount(CounterComponent);
     cy.getBySelector('count').should('have.text', '0');
   });
+
+  it('increments the counter', () => {
+    cy.mount(CounterComponent);
+    cy.getBySelector('increment-button').click();
+    cy.getBySelector('count').should('have.text', '1');
+    cy.getBySelector('increment-button').click();
+    cy.getBySelector('count').should('have.text', '2');
+  });
+
+  it('decrements the counter', () => {
+    cy.mount(CounterComponent);
+    cy.getBySelector('decrement-button').click();
+    cy.getBySelector('count').should('have.text', '-1');
+    cy.getBySelector('decrement-button').click();
+    cy.getBySelector('count').should('have.text', '-2');
+  });
 });
